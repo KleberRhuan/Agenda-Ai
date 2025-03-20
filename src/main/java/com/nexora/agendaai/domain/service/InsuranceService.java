c791cb41fac151733e251c49ac5af74e0f5e61aa
KleberRhuan <kleber_rhuan@hotmail.com>
2025-03-20 20:03:06 -0300

    chore: Removed test functions from controllers and services

--- src/main/java/com/nexora/agendaai/domain/service/InsuranceService.java
+++ src/main/java/com/nexora/agendaai/domain/service/InsuranceService.java
@@ -1,10 +1,6 @@
 package com.nexora.agendaai.domain.service;
 
-import com.nexora.agendaai.domain.dto.InsuranceDTO;
-import com.nexora.agendaai.domain.exception.InsuranceNotFoundException;
-import com.nexora.agendaai.domain.model.Insurance;
 import com.nexora.agendaai.domain.repository.InsuranceRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,17 +8,6 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class InsuranceService {
     private final InsuranceRepository insuranceRepository;
-    
-    public Insurance getInsuranceById(Long id) {
-        return insuranceRepository.findById(id).orElseThrow(() -> 
-                new InsuranceNotFoundException("Insurance not found"));
-    }
-    
-    @Transactional
-    public Insurance createInsurance(InsuranceDTO insurance) {
-        Insurance insuranceEntity = Insurance.builder().name(insurance.name()).build();
-        return insuranceRepository.save(insuranceEntity);
-    }
     
     
 }
