4399562bd616d049b94feb1e3a4e20983733b6c5
KleberRhuan <kleber_rhuan@hotmail.com>
2025-03-21 18:54:45 -0300

    feat: added get insurance by id endpoint

--- src/main/java/com/nexora/agendaai/api/controller/InsuranceController.java
+++ src/main/java/com/nexora/agendaai/api/controller/InsuranceController.java
@@ -1,10 +1,10 @@
 package com.nexora.agendaai.api.controller;
 
+import com.nexora.agendaai.domain.model.Insurance;
 import com.nexora.agendaai.domain.service.InsuranceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/insurances")
@@ -12,4 +12,10 @@ import java.util.List;
 public class InsuranceController {
     
     private final InsuranceService insuranceService;
+    
+    @GetMapping("{id}")
+    public ResponseEntity<Insurance> findById(@PathVariable Long id) {
+        Insurance insurance = insuranceService.findById(id);
+        return ResponseEntity.ok(insurance);
+    }
 }
